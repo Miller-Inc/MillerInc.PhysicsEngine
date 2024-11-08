c621a4760d5cd3b19f4e9ef32570fe8d18f2cfab
J Miller <jwmiller006@outlook.com>
2024-11-07 22:09:56 -0500

    Update gpuScene.cu

--- GPU/Cuda-Engine/gpuScene.cu
+++ GPU/Cuda-Engine/gpuScene.cu
@@ -5,20 +5,20 @@
 #include "../../include/PhysicsEngine/Scenes/Scene.h"
 #include "gpuScene.h"
 
-__global__ void processSceneKernel(Scene* scene) {
+__global__ void processSceneKernel(Scene* scene, int sceneSize) {
     int i = threadIdx.x;
-    if (i < scene->sceneObjects.size()) {
+    if (i < sceneSize) {
         // Example processing: move each object along the x-axis
-
     }
 }
 
 void GPUScene::runSceneOnGPU(Scene* scene) {
     Scene* dev_scene = nullptr;
+    int sceneSize = scene->sceneObjects.size();
     cudaMalloc((void**)&dev_scene, sizeof(Scene));
     cudaMemcpy(dev_scene, scene, sizeof(Scene), cudaMemcpyHostToDevice);
 
-    processSceneKernel<<<1, scene->sceneObjects.size()>>>(dev_scene);
+    processSceneKernel<<<1, sceneSize>>>(dev_scene, sceneSize);
 
     cudaMemcpy(scene, dev_scene, sizeof(Scene), cudaMemcpyDeviceToHost);
     cudaFree(dev_scene);
